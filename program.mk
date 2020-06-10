f6f7ec1e5a2f1a1a2c74498b3598843fa726a32d
carlost <carlos.taffe@gmail.com>
2020-06-10 00:48:48 -0300

    revisados hasta td3_freertos_05

--- program.mk
+++ program.mk
@@ -1,6 +1,15 @@
-PROGRAM_PATH = examples/c
-PROGRAM_NAME = app
-#PROGRAM_PATH = examples/c/freertos_book
-#PROGRAM_NAME = Example008
-#PROGRAM_PATH = examples/c/sapi/gpio
-#PROGRAM_NAME = blinky
+
+# Program path and name ------------------------------------------------
+
+# Program path and name default values
+PROGRAM_PATH = examples/c/td3/
+#PROGRAM_NAME = td3_barem_01
+#PROGRAM_NAME = td3_barem_02
+#PROGRAM_NAME = td3_barem_03
+#PROGRAM_NAME = td3_barem_04
+#PROGRAM_NAME = td3_barem_05
+#PROGRAM_NAME = td3_freertos_01
+#PROGRAM_NAME = td3_freertos_02
+#PROGRAM_NAME = td3_freertos_03
+#PROGRAM_NAME = td3_freertos_04
+PROGRAM_NAME = td3_freertos_05
